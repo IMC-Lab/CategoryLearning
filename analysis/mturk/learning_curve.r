8379d061005a43ee4d6f08f97fca662b76dfbb8b
kevingoneill <oneillkeving@gmail.com>
2019-03-19 14:07:16 -0400

    Added turtle stimuli

---
+++ analysis/mturk/learning_curve.r
@@ -0,0 +1,69 @@
+#!/usr/bin/Rscript
+library(ggplot2)
+library(plyr)
+
+default_filename <- 'data/cat_20190205_flower_E6.csv'
+out_dir <- 'E6_learning_curves'
+
+# accept a filename as an optional command-line argument
+args <- commandArgs(trailingOnly=T)
+if (length(args) < 1) {
+    args <- c(default_filename)
+}
+
+learning_curve <- function(trials, feature=NULL, value=NULL) {
+    # subset the data
+    fname <- paste(out_dir, 'curve', sep='/')
+    if (!is.null(feature)) {
+        fname <- paste(fname, '-', feature, sep='')
+        trials <- subset(trials, featureLearned==feature)
+        if (!is.null(value)) {
+            fname <- paste(fname, '-', value, sep='')
+            trials <- subset(trials, valueLearned==value)
+        }
+    }
+
+    nSubj <- length(unique(trials$subject))
+    nTrials <- nrow(subset(trials, subject==trials$subject[1]))
+    trials$trialNum <- rep(1:nTrials, times=nSubj)
+
+    
+    
+    png(paste(fname, '.png', sep=''), height=1000, width=1000)
+    print(ggplot(ddply(trials, c("trialNum", "isInstructed"), summarise, N=length(wasCorrect),
+                       rate=mean(wasCorrect), sd=sd(wasCorrect), se=(sd/sqrt(N)))) +
+          aes(x=trialNum, y=rate, color=isInstructed,group=isInstructed,fill=isInstructed) +
+          stat_smooth(method="loess", span=0.1, se=TRUE, alpha=0.3))
+    dev.off()
+}
+
+# analyze data from each file
+for (i in 1:length(args)) {
+    filename <- args[i]
+    writeLines(args[i])
+    
+    # import the data
+    trials.test <- subset(read.csv(filename, header=T), task=='test', select=c('subject', 'RT'))
+    trials.learn <- subset(read.csv(filename, header=T), task=='learn' & isPracticed,
+                           select=c('subject', 'isInstructed', 'featureLearned', 'valueLearned',
+                                    'wasCorrect', 'RT'))
+
+    # exclude subjects based on 3 sd's from the RT
+    exclude.test = (trials.test$RT > (mean(trials.test$RT) + 3*sd(trials.test$RT))) |
+        (trials.test$RT < (mean(trials.test$RT)-3*sd(trials.test$RT)))
+    exclude.learn = (trials.learn$RT > (mean(trials.learn$RT)+3*sd(trials.learn$RT))) |
+        (trials.learn$RT < (mean(trials.learn$RT)-3*sd(trials.learn$RT)))
+    excludedSubjects = unique(c(trials.test$subject[exclude.test],
+                                trials.learn$subject[exclude.learn]))
+    trials.learn <- trials.learn[!(trials.learn$subject %in% excludedSubjects),]
+    trials.learn$isInstructed <- as.factor(trials.learn$isInstructed)
+
+    
+    learning_curve(trials.learn)
+    for (feature in unique(trials.learn$featureLearned)) {
+        learning_curve(trials.learn, feature)
+        for (value in unique(subset(trials.learn, featureLearned==feature)$valueLearned))
+            learning_curve(trials.learn, feature, value)
+    }
+    
+}
---
+++ stimuli/turtles/drawturtles.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+
+import os, sys
+from PIL import Image, ImageColor, ImageDraw
+
+COMPONENT_DIR = 'Components'
+OUTPUT_DIR = 'images'
+OUTPUT_FILE_TYPE = '.png'
+COMPONENT_FILE_TYPE = '.png'
+IMG_W = 500
+IMG_H = 500
+
+SHELL_PREFIX = 'shell'
+SHELL_SHAPES = ['circle', 'box', 'triangle']
+SHELL_SIZE = [100, 250]
+SHELL_COLOR = (0, 0, 0) # encoded in RGB (black)
+SHELL_OFFSET = (0, 35)
+
+HEAD_PREFIX = 'head'
+HEAD_OFFSET = (0, -190)
+LEG_PREFIX = 'legs'
+LEG_OFFSET = (0,50) # move legs lower than the center
+LEG_SHAPES = ['fin', 'turtle', 'tortoise']
+LEG_COLORS = {'blue':(60, 0, 180),     # encoded in RGB
+              'pink':(190, 0, 125),
+              'yellow':(185, 160, 75),
+              'green':(0, 200, 75)}
+
+SPOT_PREFIX = 'spots'
+SPOT_COUNTS = range(1, 4)
+SPOT_COLORS = {'purple':(205, 0, 255),   # encoded in RGB
+               'orange':(255, 40, 15),
+               'brightgreen':(0, 255, 60),
+               'lightblue':(55, 175, 255)}
+SPOT_OFFSET = (0, 35)
+
+def component(name, prefix='', folder_name=COMPONENT_DIR, file_type=COMPONENT_FILE_TYPE):
+    """Get the image for the component of a given name.
+    prefix- the name of the component's feature (leg, segment, wing)
+    name- the feature's value
+    folder_name- the enclosing folder for component images
+    file_type- the format of component images"""
+    path = os.path.dirname(__file__) + '/' + folder_name + '/'
+    if prefix and name:
+        return Image.open(path + prefix + '_' + str(name) + file_type)
+    elif prefix:
+        return Image.open(path + prefix + file_type)
+    elif name:
+        return Image.open(path + name + file_type)
+
+    
+def rgb_image(rgb, w=IMG_W, h=IMG_H):
+    """Get a blank image of the color rgb"""
+    return Image.new('RGBA', (w, h), rgb)
+
+def hsv_image(hsl, w=IMG_W, h=IMG_H):
+    """Get a blank image of the color hsv"""
+    return rgb_image(ImageColor.getrgb('hsv(' + str(hsl[0]) + ', '
+                                       + str(hsl[1]) + '%, '
+                                       + str(hsl[2]) + '%)'), w=w, h=h)
+
+def draw_centered(background, img, color, offset=(0, 0)):
+    """Draw an img on a background centered around the 
+    background's center + (dx, dy)"""
+    background.paste(rgb_image(color, w=img.width, h=img.height),
+                     box=(int(offset[0] + background.width/2.0 - img.width/2.0),
+                          int(offset[1] + background.height/2.0 - img.height/2.0)),
+                     mask=img)
+
+def draw_turtle(shell_shape, spot_count, spot_color, spot_rgb,
+                leg_shape, leg_color, leg_rgb):
+    turtle = rgb_image((255,255,255))
+
+    # draw the shell
+    draw_centered(turtle, component(shell_shape, prefix=SHELL_PREFIX),
+                  SHELL_COLOR, offset=SHELL_OFFSET)
+    
+    # draw the head and legs
+    draw_centered(turtle, component('', prefix=HEAD_PREFIX), leg_rgb, offset=HEAD_OFFSET)
+    draw_centered(turtle, component(leg_shape, prefix=LEG_PREFIX),
+                  leg_rgb, offset=LEG_OFFSET)
+
+    # draw the spots
+    draw_centered(turtle, component(spot_count, prefix=SPOT_PREFIX), spot_rgb, offset=SPOT_OFFSET)
+    return turtle
+
+            
+for shell_shape in SHELL_SHAPES:
+    for spot_count in SPOT_COUNTS:
+        for spot_color, spot_rgb in SPOT_COLORS.items():
+            for leg_shape in LEG_SHAPES:
+                for leg_color, leg_rgb in LEG_COLORS.items():
+                    turtle = draw_turtle(shell_shape, spot_count, spot_color, spot_rgb,
+                                         leg_shape, leg_color, leg_rgb)
+                        
+                    # save the image to a file
+                    turtle.save(os.path.dirname(__file__) + '/' + OUTPUT_DIR
+                                + '/turtle_' + shell_shape + '_' + str(spot_count) + '_'
+                                + spot_color + '_' + leg_shape + '_' + leg_color + OUTPUT_FILE_TYPE)
+                    
